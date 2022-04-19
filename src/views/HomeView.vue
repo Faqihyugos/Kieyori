243307d20111bebf0b45c3d9402ee2271605ff67
faqihyugos <faqihzero795@gmail.com>
2022-04-19 14:32:39 +0700

    Project vue slice from html

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+ <div class="HomeView">
+   <HeaderKieyori />
+   <HeroKieyori />
+   <ProductKieyori />
+   <InstaKieyori />
+   <PartnerKieyori />
+   <FooterKieyori />
+ </div>
+</template>
+
+<script>
+
+import HeaderKieyori from "@/components/HeaderKieyori.vue";
+import HeroKieyori from "@/components/HeroKieyori.vue";
+import ProductKieyori from "@/components/ProductKieyori.vue";
+import InstaKieyori from "@/components/InstaKieyori.vue";
+import PartnerKieyori from "@/components/PartnerKieyori.vue";
+import FooterKieyori from "@/components/FooterKieyori.vue";
+
+
+export default {
+  name: "HomeView",
+  components: {
+    HeaderKieyori,
+    HeroKieyori,
+    ProductKieyori,
+    InstaKieyori,
+    PartnerKieyori,
+    FooterKieyori
+  }
+}
+</script>
